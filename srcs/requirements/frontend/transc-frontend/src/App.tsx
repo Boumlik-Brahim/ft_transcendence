68d39c2c9e80b4863047f2b94358d5d84887bf32
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-06-06 13:34:30 +0100

    SETUP:redux

--- srcs/requirements/frontend/transc-frontend/src/App.tsx
+++ srcs/requirements/frontend/transc-frontend/src/App.tsx
@@ -9,26 +9,34 @@ import { Channels } from './pages/channels';
 import { Game } from './pages/game';
 import { Leaderboard } from './pages/leaderboard';
 import { Achievements } from './pages/achievement';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store';
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <div className=''>
-          <Nav />
-          <Routes>
-            <Route path='/' element={<Landingpage/>}/>
-            <Route path='/auth' element={<Auth/>} />
-            <Route path='/chat' element={<Chat/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-            <Route path='/channels' element={<Channels/>}/>
-            <Route path='/game' element={<Game/>}/>
-            <Route path='/leaderboard' element={<Leaderboard/>}/>
-            <Route path='/achievement' element={<Achievements/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <div className=''>
+            <Nav />
+            <Routes>
+              <Route path='/' element={<Landingpage/>}/>
+              <Route path='/auth' element={<Auth/>} />
+              <Route path='/chat' element={<Chat/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+              <Route path='/channels' element={<Channels/>}/>
+              <Route path='/game' element={<Game/>}/>
+              <Route path='/leaderboard' element={<Leaderboard/>}/>
+              <Route path='/achievement' element={<Achievements/>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </PersistGate>
+      </Provider>
     </>
   )
 }
