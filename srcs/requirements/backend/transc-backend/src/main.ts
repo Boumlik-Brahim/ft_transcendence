47c88b64d8c660b9accba6087f8c9cce7a1a5f30
B-omayr <iomayr@student.1337.ma>
2023-06-20 08:40:02 +0100

    new: add a validationPipe to the main function

--- srcs/requirements/backend/transc-backend/src/main.ts
+++ srcs/requirements/backend/transc-backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +15,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }
+  )),
   await app.listen(3000);
 }
 bootstrap();
