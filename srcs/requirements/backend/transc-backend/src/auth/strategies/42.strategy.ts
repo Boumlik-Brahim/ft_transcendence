c5689d56dc3bd2ff80cda384bfe3835deca8880c
oussamaqatim <oqatim@student.1337.ma>
2023-06-21 14:32:51 +0100

    update: change the logic of the strategy

--- srcs/requirements/backend/transc-backend/src/auth/strategies/42.strategy.ts
+++ srcs/requirements/backend/transc-backend/src/auth/strategies/42.strategy.ts
@@ -1,59 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from 'passport-42';
+// import { UserDto } from "src/users/dto/user.dto";
+import { ConfigService } from "@nestjs/config";
+import { AuthDto } from "../dto/auth.dto";
+
 @Injectable()
-export class AuthService {
-  // Other methods and dependencies
+export class IntraStrategy extends PassportStrategy(Strategy, '42') {
 
-  async signupWith42(code: string): Promise<User> {
-    // Make a POST request to the 42 API's token endpoint to exchange the authorization code for an access token
-    const tokenEndpoint = 'https://api.intra.42.fr/oauth/token';
-    const clientId = this.configService.get('42.clientId');
-    const clientSecret = this.configService.get('42.clientSecret');
-    const redirectUri = this.configService.get('42.redirectUri');
+    constructor(private configService: ConfigService) {
+        super({
+            clientID: configService.get('CLIENT_ID'),
+            clientSecret: configService.get('SECRET'),
+            callbackURL: configService.get('REDIRECT_URI'),
+            profileFields: {
+                'username': 'login',
+                'emails.value': 'email',
+            }
+        }); // Config
+    }
 
-    const response = await this.http
-      .post(tokenEndpoint, {
-        grant_type: 'authorization_code',
-        client_id: clientId,
-        client_secret: clientSecret,
-        code,
-        redirect_uri: redirectUri,
-      })
-      .toPromise();
+    async validate(accessToken: string, refreshToken: string, profile: any, done: Function): Promise<any> {
+        const {username, emails} = profile;
+        const user: AuthDto = {
+            username: username,
+            email: emails.value,
+        }
+        done(null, user);
+    }
 
-    const accessToken = response.access_token;
-
-    // Use the access token to make a GET request to the 42 API's /me endpoint to retrieve user information
-    const meEndpoint = 'https://api.intra.42.fr/v2/me';
-
-    const userResponse = await this.http
-      .get(meEndpoint, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .toPromise();
-
-    // Process the user data and create or update the user in your application's database
-    const user = this.processUserResponse(userResponse);
-
-    // Return the created or updated user
-    return user;
-  }
-
-  private processUserResponse(response: any): User {
-    // Extract relevant user information from the response and create or update the user in your database
-    // You may need to map or transform the data to fit your User model or schema
-
-    // Example:
-    const user: User = {
-      id: response.id,
-      username: response.login,
-      email: response.email,
-      // Other user properties
-    };
-
-    // Save the user in your database or update existing user
-    // ...
-
-    return user;
-  }
 }
