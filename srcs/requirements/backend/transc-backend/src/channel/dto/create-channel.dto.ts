631ec8baf92a859de09439836610bb0ea99288b2
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-05 18:13:14 +0100

    ADD: create channel DTO

--- srcs/requirements/backend/transc-backend/src/channel/dto/create-channel.dto.ts
+++ srcs/requirements/backend/transc-backend/src/channel/dto/create-channel.dto.ts
@@ -1,1 +1,25 @@
-export class CreateChannelDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateChannelDto {
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    channelName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    channelType: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    channelPassword: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    channelOwnerId: string;
+}
