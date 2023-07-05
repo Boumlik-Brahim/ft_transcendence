c030c0c548a5740d0b98e1988da05769cacc0850
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-05 18:13:40 +0100

    ADD: update channel DTO

--- srcs/requirements/backend/transc-backend/src/channel/dto/update-channel.dto.ts
+++ srcs/requirements/backend/transc-backend/src/channel/dto/update-channel.dto.ts
@@ -1,4 +1,22 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateChannelDto } from './create-channel.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class UpdateChannelDto extends PartialType(CreateChannelDto) {}
+export class UpdateChannelDto extends PartialType(CreateChannelDto) {
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    channelName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    channelType: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    channelPassword: string;
+}
