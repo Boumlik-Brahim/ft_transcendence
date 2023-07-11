865b98e12f19fe14bbe74dc1bd8fa8081476fafa
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-11 14:32:47 +0100

    FIX: main merge problems

--- srcs/requirements/backend/transc-backend/src/game/game.service.ts
+++ srcs/requirements/backend/transc-backend/src/game/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'prisma/prisma.service';
 import { GameEntity } from './entity/game.entity';
 import { v4 as uuidv4 } from 'uuid';
 
