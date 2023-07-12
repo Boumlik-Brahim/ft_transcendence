1e763feca7b42b8709fabcaf0ebaa2dfd53bb52a
Fode Oulare <foulare@e3r1p6.1337.ma>
2023-07-12 19:30:36 +0100

    game frontend

--- srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/game/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/game/page.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 import Popup from './popup'
 import Player from './player';
 import Friends from './friends';
-import Sidebar from '../../../../components/Sidebar';
+import { OnlineFriends } from '../../../../constant';
+import Image from 'next/image';
 
 const Game = () => {
   const [isOpen, setClose] = useState<boolean>(false)
@@ -11,11 +12,11 @@ const Game = () => {
   return (
       <div className='flex items-start justify-center lg:gap-2 w-full h-[100vh]  '>
         <div className='mt-5 h-full w-full lg:w-[70%]'>
-          <h1 className='text-[30px] text-center text-primary m-10 font-bold'>Play</h1>
+          <h1 className='text-[30px] text-center text-primary m-10 font-bold game_font'>Play</h1>
           <div className='md:flex items-sart justify-center gap-2 m-auto'>
             <div className='flex flex-col-reverse md:flex-col'>
               <Player playerId={1} inputValue={oponentName} setInputValue={setOponentName} />
-              <button className='border-2 gradient-bg shadow-xl ml-auto mr-auto md:mr-0 text-primary text-[20px] p-2 w-[268px] h-[61px] rounded-[40px] md:ml-4 mt-10 hover:bg-primary ease-in duration-300 hover:text-white hover:border-none'>
+              <button className='border-2 gradient-bg shadow-xl ml-auto mr-auto md:mr-0 text-primary text-[20px] p-2 w-[268px] h-[61px] rounded-[40px] md:ml-4 mt-10 hover:bg-primary ease-in duration-300 hover:text-white hover:border-none' onClick={() => setClose(true)}>
                 Random player
               </button>
             </div>
@@ -26,10 +27,26 @@ const Game = () => {
           </div>
         </div>
         <div className='w-[30%] h-full hidden lg:flex justify-center items-center '>
-          <div className='h-[90%] m-2 border w-[400px] rounded-[40px] gradient-bg '>
-
+          <div className='h-[90%] m-2 border w-[400px] rounded-[40px] gradient-bg p-4 '>
+            <h1 className='game_font mt-5 text-center '> Online  Players ({OnlineFriends?.length})</h1>
+            <div className='mt-10'>
+              {
+                OnlineFriends.map((user, index) => (
+                  <div className='w-full flex justify-between items-center mt-5' key={index}>
+                    <div className='flex gap-3 items-center w-[50%]'>
+                      <div className='h-auto m-auto'>
+                        <Image src={user.avarat} height='61' width='61' alt='no player' />
+                      </div>
+                      <h1 className='text-primary text-[12px] font-[700]'>{user.name}</h1>
+                    </div>
+                    <button className='p-1 border text-[10px] text-primary border-primary rounded-[40px] w-[80px] hover:bg-primary hover:text-white'>challenge</button>
+                  </div>
+                ))
+              }
+            </div>
           </div>
         </div>
+        <Popup isOpen={isOpen} setClose={setClose}></Popup>
       </div>
 
   )
--- srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/game/popup.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/game/popup.tsx
@@ -23,24 +23,6 @@ const Popup = ({isOpen, setClose} : Props) => {
                         className='absolute right-2 cursor-pointer top-2 block rounded'>
                         <Image width="30" height="30" src='/closesquare.svg' alt="cross"/>
                     </span>
-                    <div className='md:flex items-center h-auto md:h-[400px]  gap-2 w-full mt-20'>
-                        <div className='md:w-[40%] p-2 mt-40 h-full'>
-                            {/* <h1 className='text-center mp-5 text-[30px]  font-[10px] text-primary mb-10'>HUMAN</h1> */}
-                            <form className=''>
-                                <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='block m-auto w-[252px] p-2 h-[47px] border-2 file:border-solid border-primary rounded' placeholder='Search' />
-                            </form>
-                            <Friends _name={inputValue} setName={setInputValue}></Friends>
-                        </div>
-                        <div className='md:w-[20%] flex justify-center items-center p-2 h-[80%]'>
-                            <div className='h-[300px] w-[1px] border-dashed border border-primary m-auto mt-auto'>
-                            </div>                               
-                            {/* <div className='h-[140px] w-[104px] left bg-white  text-center border-dashed absolute m-auto rounded-lg outline-dashed'>
-                            </div> */}
-                        </div>
-                        <div className='md:w-[40%] p-2  md:mt-40 h-full text-center'>
-                            <button className='p-3 bg-primary w-[200px] text-white rounded-lg m-auto'>JOIN A QUEUE</button>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
