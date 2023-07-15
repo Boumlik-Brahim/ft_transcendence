23e2e74100dad669de2f1972b0cbc74c4a12af3a
B-omayr <iomayr@student.1337.ma>
2023-07-15 17:29:54 +0100

    new: add cookieParser to the main

--- srcs/requirements/backend/transc-backend/src/main.ts
+++ srcs/requirements/backend/transc-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
