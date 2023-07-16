183304131b82d2468b61cef5570219bb1112de88
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-16 16:33:46 +0100

    EDIT: app module

--- srcs/requirements/backend/transc-backend/src/app.module.ts
+++ srcs/requirements/backend/transc-backend/src/app.module.ts
@@ -3,14 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { ChatModule } from './chat/chat.module';
 import { ChannelModule } from './channel/channel.module';
 import { GameModule } from './game/game.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [PrismaModule, GameModule, ChatModule, ChannelModule, AuthModule, UsersModule],
-  // imports: [PrismaModule, GameModule, ChatModule, ChannelModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
