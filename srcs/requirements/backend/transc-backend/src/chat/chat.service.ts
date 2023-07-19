cb2accc0abd4f4f63269ae241e1ce6f6d3d83b5f
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-19 18:26:00 +0100

    EDIT: createChat and findAllChats ,ADD: updateChat and countUnreadMessages

--- srcs/requirements/backend/transc-backend/src/chat/chat.service.ts
+++ srcs/requirements/backend/transc-backend/src/chat/chat.service.ts
@@ -2,13 +2,25 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { DirectMessage } from '@prisma/client';
+import { createHash } from 'crypto';
+import { UpdateChatDto } from './dto/update-chat.dto';
 
 @Injectable()
 export class ChatService {
   constructor(private prisma: PrismaService) {}
 
   async createChat(createChatDto: CreateChatDto): Promise<DirectMessage> {
-    return this.prisma.directMessage.create({ data: createChatDto })
+  
+    const roomID = [createChatDto.senderId, createChatDto.recieverId].sort().join('-');
+    const hasshedRoomName = createHash('sha256').update(roomID).digest('hex');
+    console.log(hasshedRoomName);
+  
+    return this.prisma.directMessage.create({ data: {
+      content: createChatDto.content,
+      senderId: createChatDto.senderId,
+      recieverId: createChatDto.recieverId,
+      roomId: hasshedRoomName
+    } })
     .catch (error => {
       throw new HttpException({
         status: HttpStatus.BAD_REQUEST,
@@ -18,16 +30,16 @@ export class ChatService {
       });
     });
   }
-
+  
   async findAllChats(senderId: string, receiverId: string): Promise<DirectMessage[]> {
+  
+    const roomID = [senderId, receiverId].sort().join('-');
+    const hasshedRoomName = createHash('sha256').update(roomID).digest('hex');
+    console.log(hasshedRoomName);
+
     return this.prisma.directMessage.findMany({
       where: {
-        sender: {
-          id: senderId,
-        },
-        reciever: {
-          id: receiverId,
-        },
+        roomId: hasshedRoomName
       },
       orderBy: {
         created_at: 'asc',
@@ -44,10 +56,10 @@ export class ChatService {
   }
 
   async findOneChat(id: string): Promise<DirectMessage> {
-    return this.prisma.directMessage.findUnique({ where: 
-      {
+    return this.prisma.directMessage.findUnique({
+      where: {
         id
-      } 
+      }
     })
     .catch (error => {
       throw new HttpException({
@@ -57,5 +69,38 @@ export class ChatService {
         cause: error
       });
     })
+  }
+
+  async updateChat(messageID: string, updateChatDto: UpdateChatDto): Promise<DirectMessage> {
+    return this.prisma.directMessage.update({
+      where: {
+        id: messageID,
+      },
+      data: updateChatDto
+    })
+    .catch (error => {
+      throw new HttpException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'InternalServerErrorException',
+      }, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: error
+      });
+    });
+  }
+
+  async countUnreadMessages(senderId: string, receiverId: string){
+
+    const roomID = [senderId, receiverId].sort().join('-');
+    const hasshedRoomName = createHash('sha256').update(roomID).digest('hex');
+  
+    const unreadMessages = await this.prisma.directMessage.count({
+      where: {
+        recieverId: receiverId,
+        roomId: hasshedRoomName,
+        seen: false
+      },
+    });
+    
+    return unreadMessages;
   }
 }
