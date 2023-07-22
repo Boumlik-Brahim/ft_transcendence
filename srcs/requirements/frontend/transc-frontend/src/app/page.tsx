450bba0442021563e9c72bf5d72397ef5b1bb0ea
B-omayr <iomayr@student.1337.ma>
2023-07-22 18:35:23 +0100

    new: landing page completed

--- srcs/requirements/frontend/transc-frontend/src/app/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/page.tsx
@@ -1,13 +1,246 @@
-export const metadata = {
-  title: 'Transcendence',
-}
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import cookie from "js-cookie";
+import InfoBox from "../../components/Features";
+import Member from "../../components/Member";
 
-function landing_page() {
+import Image from "next/image";
+export default function Page() {
   return (
-    <div>
-      landing page
-    </div> 
-  )
+    <>
+      <section className="w-full h-[80vh] bg-gray-300">
+        <div className="w-full h-[100%]">
+          <div className="w-full h-[100%] flex justify-center ">
+            <div className=" h-[80vh] w-[2px] border-[6px] border-dashed absolute"></div>
+            <div
+              className="
+              w-full h-[20%]  flex items-center justify-between pt-20 p-10 z-20
+              md:h-[30%] md:px-[15%]"
+            >
+              {/* <div className="w-full h-full flex items-center justify-between pt-20 p-10"> */}
+                <Image
+                  src={"/logo_b.svg"}
+                  alt="logo"
+                  width={67}
+                  height={67}
+                  className="w-[67px] h-[67px] md:w-[110px] md:h-[110px]"
+                />
+                <div className="
+                    w-[101px] h-[39px] font-press text-[10px] rounded-full text-[#3E3B6A] bg-white flex items-center justify-center
+                    md:w-[150px] md:h-[70px] md:text-[18px]"
+                  >
+                  <Link href="/login">
+                    login
+                  </Link>
+                </div>
+              {/* </div> */}
+            </div>
+            <div
+              className="
+              w-[390px] h-[20%] flex items-center justify-center absolute z-20
+              md:w-[651px] md:h-[18%] md:mt-[80px]"
+            >
+              <h1
+                className="
+                w-[100%] h-[20%]  font-press text-[15px] text-center z-10 text-[#3E3B6A]
+                md:text-[33px]"
+              >
+                WELCOME TO THE MOST IMPRESSIVE TRANSCENDANCE IN THE WORLD
+              </h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-full text-white bg-[#3E3B6A] ">
+        <div
+          className="
+          w-full h-[80px] text-[35px] flex justify-center items-center
+          md:text-[50px]
+          lg:text-[100px] lg:h-[200px]"
+        >
+          <h1 className="font-press ">Features</h1>
+        </div>
+        <InfoBox
+          title="PLAY ONLINE"
+          paragraph="Playing Ping Pong online, also known as table tennis, involves playing a digital version of the sport over the internet with another player."
+          image="/landing/CONTROLER.svg"
+          isChat={false}
+        />
+        <InfoBox
+          title="PLAY AND CHAT"
+          paragraph="Chatting during gameplay can add a social element to the game, allowing players to communicate with each other and build connections even if they are playing from different locations."
+          image="/landing/CHAT.svg"
+          isChat={true}
+        />
+        <InfoBox
+          title="MAKE FRIENDS"
+          paragraph="The game provides a great opportunity for players to meet and connect with other people who share a love for the sport.
+          players can use the chat feature to communicate with other players."
+          image="/landing/FRIENDS.svg"
+          isChat={false}
+        />
+        <div
+          className="
+          w-full h-[50px]
+          md:hidden"
+        ></div>
+      </section>
+      <section className="w-full bg-gray-300 text-[#3E3B6A] font-roboto">
+        <div className="w-full h-[100px] flex justify-center items-center">
+          <h1 className="font-press text-[35px]">Our Team</h1>
+        </div>
+        <div
+          className="
+          w-full flex flex-col items-center
+          md:flex-row justify-center flex-wrap"
+        >
+          <Member
+            img="/fake/avatar.png"
+            name="Breezy"
+            job="Full Stack"
+            paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            intraUrl="https://profile.intra.42.fr/users/iomayr"
+            gitUrl="https://github.com/B-omayr"
+          />
+          <Member
+            img="/fake/avatar.png"
+            name="Boumlik"
+            job="string"
+            paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            intraUrl="string"
+            gitUrl="string"
+          />
+          <Member
+            img="/fake/avatar.png"
+            name="Bilal"
+            job="string"
+            paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            intraUrl="string"
+            gitUrl="string"
+          />
+          <Member
+            img="/fake/avatar.png"
+            name="Foulare"
+            job="string"
+            paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            intraUrl="string"
+            gitUrl="string"
+          />
+          <Member
+            img="/fake/avatar.png"
+            name="OUssama"
+            job="string"
+            paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            intraUrl="string"
+            gitUrl="string"
+          />
+        </div>
+      </section>
+      <footer className="w-full h-[50vh] bg-[#3E3B6A] text-white">
+        <div className="w-full h-[60%] flex justify-center">
+          <Image
+            src={"/logo_w.svg"}
+            alt="logo"
+            width={100}
+            height={80}
+            className="md:w-[150px]"
+          />
+        </div>
+        <h3
+          className="
+          text-center font-roboto
+          md:text-[25px]"
+        >
+          Website Made By Hard Work 💪 And Love ❤️
+        </h3>
+        <div
+          className="
+          flex justify-center mt-[15px]
+          md:mt-[40px]"
+        >
+          <hr className="w-[90%] border-t border-gray-300" />
+        </div>
+        <h4
+          className="
+          text-center mt-[20px] text-[20px]
+          md:mt-[40px] md:text-[30px]"
+        >
+          Copyright
+        </h4>
+      </footer>
+    </>
+  );
 }
 
-export default landing_page
+// const LoginButton = () => {
+
+//   // const e = cookie.get('id');
+//   // console.log(e);
+
+//   // const [loginStatus, setLoginStatus] = useState<boolean | undefined>(undefined);
+
+//   // const handleLoginClick = async () => {
+//   //   try {
+//   //     const response = await fetch('http://localhost:3000/auth', {
+//   //       method: 'GET',
+//   //     });
+
+//   //     setLoginStatus(true);
+//   //   }
+//   //   catch (error) {
+//   //     console.error('Error during login:', error);
+//   //     setLoginStatus(false);
+//   //   }
+//   // };
+//   // return (
+//   //   <div>
+//   //   {/* {loginStatus === undefined ? ( */}
+//   //     <Link href="http://localhost:3000/auth">LoG In</Link>
+//   //     {/* // <button className="border-8 rounded" onClick={handleLoginClick}>
+//   //     //   Log In
+//   //     // </button> */}
+//   //   {/* // ) : loginStatus ? (
+//   //   //   <p>Login successful!</p>
+//   //   // ) : (
+//   //   //   <p>Login failed. Please try again.</p>
+//   //   // )} */}
+//   // </div>
+//   // )
+// }
+
+// export default LoginButton
+
+      // <section className="w-full h-[80vh]">
+      //   <div className="w-full h-[80vh] bg-channel-100">
+      //     <div className="w-full h-[100%] relative">
+      //       <div className="z-15 w-[100%] h-[20%]  font-press text-[15px] text-center z-10 pt-[50%] text-[#3E3B6A]">
+      //         <h1>WELCOME TO THE MOST IMPRESSIVE TRANSCENDANCE IN THE WORLD</h1>
+      //       </div>
+      //     </div>
+      //       <div className="w-full flex justify-center items-center ">
+      //         <div className=" h-[80vh] w-[2px] border-[6px] border-dashed  flex items-center"></div>
+      //       </div>
+      //   </div>
+      //   {/* <div className="w-ful h-[100%] bg-gray-300 flex items-center justify-center relative">
+      //     <div className=" h-[100%] w-[2px] border-[6px] border-dashed absolute "></div>
+      //     <div className="w-full h-[20%] bg-orange-500">div1</div> 
+      //       <div className="w-full flex flex-col ">
+      //       <div className="w-full flex items-center justify-between pt-20 p-10">
+      //         <Image src={"/logo_b.svg"} alt="logo" width={67} height={67} />
+      //         <Link
+      //           href=""
+      //           className="w-[101px] h-[39px] font-press text-[10px] rounded-full p-4 text-[#3E3B6A] bg-white flex items-center justify-center"
+      //         >
+      //           Log In
+      //         </Link>
+      //       </div>
+            // <div className="w-full h-[88px] flex items-center justify-center">
+            //   <h1 className="block w-[100%] h-[20%]  font-press text-[15px] text-center z-10 text-[#3E3B6A]">
+            //     WELCOME TO THE MOST IMPRESSIVE TRANSCENDANCE IN THE WORLD
+            //   </h1>
+              
+            // </div>
+      //     </div> 
+      //   </div> */}
+      // </section>
