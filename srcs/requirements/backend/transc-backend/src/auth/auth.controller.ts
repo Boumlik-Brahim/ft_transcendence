4d9c9cb1398a1bc8aec3ba7d3439a84afc3994df
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-24 10:06:41 +0100

    ADD: register, turnOnTwoFactorAuthentication, authenticate 2fa crud post operations

--- srcs/requirements/backend/transc-backend/src/auth/auth.controller.ts
+++ srcs/requirements/backend/transc-backend/src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Req, Res, Get, UseGuards, Redirect } from "@nestjs/common";
+import { Controller, Req, Res, Get, UseGuards, Redirect, Post, Body, UnauthorizedException, Response, Query } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto/auth.dto"
-import { Response } from "express";
 import { AuthGuard } from "@nestjs/passport";
 import { JwtPayload } from "./type/jwt-payload.type"
+import { ApiTags } from "@nestjs/swagger";
+import { UsersService } from "src/users/users.service";
 // import { Request } from 'express';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-    constructor (private authService: AuthService) {}
+    constructor (private authService: AuthService,
+        private userService: UsersService) {}
 
     @Get()
     @UseGuards(AuthGuard('42'))
@@ -22,5 +25,32 @@ export class AuthController {
         const token = await this.authService.signToken(payload);
         res.cookie('accessToken', token);
         return res.redirect('http://localhost:3000/')
+    }
+    
+    @Post('2fa/generate')
+    async register(@Query('userId') userId: string, @Res() res: any) {
+        const user = await this.userService.findOne(userId);
+        const { otpauthUrl } = await this.authService.generateTwoFactorAuthenticationSecret(user);
+        return res.json( await this.authService.generateQrCodeDataURL(otpauthUrl));
+    }
+    
+    @Post('2fa/turn-on')
+    async turnOnTwoFactorAuthentication(@Query('userId') userId: string, @Query('authCode') authCode: string){
+        const user = await this.userService.findOne(userId);
+        const isCodeValid = this.authService.isTwoFactorAuthenticationCodeValid(authCode, user);
+        if (!isCodeValid){
+            throw new UnauthorizedException('Wrong authentication code');
+        }
+        await this.authService.turnOnTwoFactorAuthentication(user.id);
+    }
+    
+    @Post('2fa/authenticate')
+    async authenticate(@Query('userId') userId: string, @Query('authCode') authCode: string){
+        const user = await this.userService.findOne(userId);
+        const isCodeValid = this.authService.isTwoFactorAuthenticationCodeValid(authCode, user);
+        if (!isCodeValid){
+            throw new UnauthorizedException('Wrong authentication code');
+        }
+        return this.authService.loginWith2fa(user);
     }
 }
