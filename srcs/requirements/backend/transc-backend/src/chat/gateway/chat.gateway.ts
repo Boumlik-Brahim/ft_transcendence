2c27af0beefda46c2071a1738146d994bd9908e0
Boumlik Brahim <boumlik01brahim@gmail.com>
2023-07-27 09:47:29 +0100

    EDIT: chat Gateway

--- srcs/requirements/backend/transc-backend/src/chat/gateway/chat.gateway.ts
+++ srcs/requirements/backend/transc-backend/src/chat/gateway/chat.gateway.ts
@@ -2,8 +2,8 @@ import { Logger } from '@nestjs/common';
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from '../chat.service';
-import { createHash } from 'crypto';
 import { CreateChatDto } from '../dto/create-chat.dto';
+import { connectedClients } from 'src/app.gateway';
 
 @WebSocketGateway( {
   cors: { 
@@ -19,7 +19,17 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   @WebSocketServer()
   server: Server ;
 
-  private logger: Logger = new Logger('Gateway Log');
+  private logger: Logger = new Logger('CHAT Gateway Log');
+
+  isUserConnected(userId: string): boolean {
+    return connectedClients.has(userId);
+  }
+  
+  async getSocketIdByUserId(userId: string): Promise<string> {
+    if (this.isUserConnected(userId)){
+      return connectedClients.get(userId);
+    }
+  }
 
   afterInit(server: Server) {
     this.logger.log('Initialized!');
@@ -30,47 +40,43 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   }
 
   @SubscribeMessage('joinRoom')
-  handleJoinRoom(@MessageBody() payload: { senderId: string, recieverId: string }, @ConnectedSocket() socket: Socket): void {
-    const { senderId, recieverId } = payload;
-    const roomID = `${senderId}-${recieverId}`;
-    const hasshedRoomName = createHash('sha256').update(roomID).digest('hex');
-    console.log(`hashedRoomName===> ${hasshedRoomName}`);
-    
+  async handleJoinRoom(@MessageBody() payload: { senderId: string, recieverId: string }, @ConnectedSocket() socket: Socket): Promise<void> {
+    const hasshedRoomName = await this.chatService.generateHashedRommId(payload.senderId, payload.recieverId);
+
     Array.from(socket.rooms)
     .filter((id) => id !== socket.id)
     .forEach((id) => {
       socket.leave(id);
     });
-    
+
     if (!socket.rooms.has(hasshedRoomName)){
       socket.join(hasshedRoomName);
+      this.server.to(socket.id).emit('joined', {roomName: hasshedRoomName});
+      this.logger.log(`joinRoom: ${socket.id} joined ${hasshedRoomName}`);
     }
-    
-    console.log('Array from');
-    console.log(Array.from(socket.rooms));
-    this.logger.log(`joinRoom: ${socket.id} joined ${hasshedRoomName}`);
-    this.server.to(hasshedRoomName).emit('joined', 'khuna joina');
-    // await this.chatService.createChat(payload);
   }
   
   @SubscribeMessage('message')
   async handleEvent(@MessageBody() payload: CreateChatDto, @ConnectedSocket() socket: Socket): Promise<void> {
-    // const roomID = [payload.senderId, payload.recieverId].sort().join('-');
-    // const hasshedRoomName = createHash('sha256').update(roomID).digest('hex');
     const hasshedRoomName = await this.chatService.generateHashedRommId(payload.senderId, payload.recieverId);
-    console.log(`hashedRoomName===> ${hasshedRoomName}`);
+    Array.from(socket.rooms)
+    .filter((id) => id !== socket.id)
+    .forEach((id) => {
+      socket.leave(id);
+    });
 
-    console.log(Array.from(socket.rooms));
-    
     if (!socket.rooms.has(hasshedRoomName)){
       socket.join(hasshedRoomName);
       this.logger.log(`joinRoom: ${socket.id} joined ${hasshedRoomName}`);
     }
 
-    this.server.to(hasshedRoomName).emit('sendmessage', payload.content);
+    this.server.to(hasshedRoomName).emit('getMessage',{ senderId: payload.senderId, receiverId: payload.recieverId, text: payload.content, room: hasshedRoomName});
+    // const receiverSocketId = this.getSocketIdByUserId(payload.recieverId);
+    // const sender = await this.
+    // this.server.to(receiverSocketId).emit('messageNotif', );
     await this.chatService.createChat(payload);
   }
-
+  
   handleDisconnect(client: Socket): void {
     this.logger.log(`Client disconnected: ${client.id}`);
   }
