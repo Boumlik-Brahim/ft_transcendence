83b894bfae24cf3f222366f508b75488ece3d460
B-omayr <iomayr@student.1337.ma>
2023-07-27 12:52:30 +0100

    update: avoid working with js-cookie

--- srcs/requirements/frontend/transc-frontend/src/app/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
-"use client";
+'use client'
 import React, { useState } from "react";
 import Link from "next/link";
-import cookie from "js-cookie";
 import InfoBox from "../../components/Features";
 import Member from "../../components/Member";
 
