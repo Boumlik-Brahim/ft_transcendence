4fd4a277e2ceb1137bb2d0131df56940dfd06063
Boumlik Brahim <92986664+Boumlik-Brahim@users.noreply.github.com>
2023-07-29 18:21:22 +0100

    ADD: ConnectedClientsService to game module providers

--- srcs/requirements/backend/transc-backend/src/game/game.module.ts
+++ srcs/requirements/backend/transc-backend/src/game/game.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { GameService } from './game.service';
 import { GameGateway } from './game.gateway';
+import { ConnectedClientsService } from 'src/connected-clients.service';
 
 @Module({
-  providers: [GameService, GameGateway],
+  providers: [GameService, GameGateway, ConnectedClientsService],
   imports : [PrismaModule]
 })
 export class GameModule {}
