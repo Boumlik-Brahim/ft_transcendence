e9dc56a3778ba6607dee3785ae5554828a953f5b
B-omayr <iomayr@student.1337.ma>
2023-08-01 18:15:38 +0100

    update: push Setting page before set the qrCode in Popup

--- srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/setting/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/setting/page.tsx
@@ -9,36 +9,87 @@ import Cookies from 'universal-cookie';
 function page() {
   const cookies = new Cookies();
   const userId = cookies.get('id');
+  const [enterInput, setEnterInput] = useState("hidden");
   const [qrCodeActive, setQrCode] = useState<boolean>(false);
   const [disableTwoFa, setDisableTwoFa] = useState<boolean>(false);
+  const [checkIfTwoFa, setCheckIfTwoFa] = useState<boolean>(false);
+
+  const fetchData = async () => {
+    try {
+      const userData = await fetch(`http://localhost:3000/users/${userId}`);
+      if (userData.ok){
+        const user = await userData.json();
+        if(user.isTwoFactorEnabled){
+          setDisableTwoFa(false);
+          await fetch(`http://localhost:3000/users/${userId}/two-factor`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ isTwoFactorEnabled: false }), 
+          });;
+        }
+      }
+      else{
+        console.error('Failed to fetch user data');
+      }
+    }
+    catch(error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
 
   const handleActiveClick = () => {
     setQrCode(true);
+    setEnterInput("");
+    fetchData();
   }
+  
+  const [formData, setFormData] = useState({
+    inputField1: '',
+    inputField2: '',
+    inputField3: '',
+    inputField4: '',
+    inputField5: '',
+    inputField6: '',
+  });
+  
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => {
+      console.log(e.target.name);
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const user = await fetch(`http://localhost:3000/users/${userId}`);
-        if (user.ok){
-          const userData = await user.json();
-          if(userData.isTwoFactorEnabled)
-            setDisableTwoFa(true);
-        }
-        else{
-          console.error('Failed to fetch user data');
-        }
-      }
-      catch(error) {
-        console.error('Error fetching user data:', error);
+    async function check () {
+      const userData = await fetch(`http://localhost:3000/users/${userId}`);
+      const user = await userData.json();
+      if (user.isTwoFactorEnabled){
+        setDisableTwoFa(true);
+        setCheckIfTwoFa(false);
+        setEnterInput("hidden");
+        setQrCode(false);
       }
     }
-
-    fetchData();
-  },[userId]);
+    check();
+  }, [checkIfTwoFa])
+  
+  const handleSaveClick = async () => {
+    console.log('Form Data:', formData);
+    fetch(`http://localhost:3000/auth/2fa/turn-on?userId=${userId}&authCode=${formData}`, {
+      method: 'POST',
+    })
+    setCheckIfTwoFa(true);
+  };
 
   return (
     <div className="layouts">
-      <div className='px-[20%] w-full h-[80vh] flex flex-col justify-evenly item-center text-[#3E3B6A] md:h-[100vh]'>
+      <div className='px-[20%] w-full h-[80vh] flex flex-col justify-start item-center text-[#3E3B6A] md:h-[100vh]'>
         <div className='flex justify-center item-center font-press'>
           <h1>Edit Profile</h1>
         </div>
@@ -64,7 +115,7 @@ function page() {
             <input 
               type="text"
               id="username"
-              name=""
+              name="username"
               className='border'> 
               </input>
           </form>
@@ -73,27 +124,101 @@ function page() {
           <h3 className='flex justify-center item-center'>2FA-AUTHENTICATOR</h3>
           {
             disableTwoFa ? (
-              <button onClick={handleActiveClick} className='w-[90%] h-[30px] text-white self-center text-center bg-[#3E3B6A] border rounded-lg'>ACTIVE</button>
+              <button onClick={handleActiveClick} className='w-[90%] h-[30px] text-white self-center text-center bg-[#3E3B6A] border rounded-lg'>Desactivate</button>
               ) : (
-                <button onClick={handleActiveClick} className='w-[90%] h-[30px] text-white self-center text-center bg-[#3E3B6A] border rounded-lg'>Desactivate</button>
+                <button onClick={handleActiveClick} className='w-[90%] h-[30px] text-white self-center text-center bg-[#3E3B6A] border rounded-lg'>ACTIVE</button>
             )
           }
         </div>
-        <div className=' w-[80%] self-center flex justify-center item-center border-2 rounded-lg'>
-          {
-            qrCodeActive ? (
-              <QRCodePage 
-                userId={userId}
-                blur=""/>
-              ) : (
+        <div className='flex flex-col items-center'>
+          <div className=' w-[80%] self-center flex justify-center item-center border-2 rounded-lg'>
+            {
+              qrCodeActive ? (
                 <QRCodePage 
                   userId={userId}
-                  blur="blur-sm"/>
-            )
-          }
+                  blur=""/>
+                ) : (
+                  <QRCodePage 
+                    userId={userId}
+                    blur="blur-sm"/>
+              )
+            }
+          </div>
+          <form action="" method="post" className={`${enterInput}`}>
+              <div className="flex space-y-16">
+                <div className="flex flex-row items-center justify-between mx-auto w-full max-w-md">
+                  <div className="w-12 h-12 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                      type="number"
+                      name="inputField1"
+                      id="inputField1"
+                      max="1"
+                      onChange={handleChange}
+                      value={formData.inputField1}
+                    ></input>
+                  </div>
+                  <div className="w-12 h-12 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                      type="number"
+                      name="inputField2"
+                      id="inputField2"
+                      max="1"
+                      onChange={handleChange}
+                      value={formData.inputField2}
+                    ></input>
+                  </div>
+                  <div className="w-12 h-12 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                      type="number"
+                      name="inputField3"
+                      id="inputField3"
+                      max="1"
+                      onChange={handleChange}
+                      value={formData.inputField3}
+                    ></input>
+                  </div>
+                  <div className="w-12 h-12 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                      type="number"
+                      name="inputField4"
+                      id="inputField4"
+                      max="1"
+                      onChange={handleChange}
+                      value={formData.inputField4}
+                    ></input>
+                  </div>
+                  <div className="w-12 h-12 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                      type="number"
+                      name="inputField5"
+                      id="inputField5"
+                      max="1"
+                      onChange={handleChange}
+                      value={formData.inputField5}
+                      ></input>
+                  </div>
+                  <div className="w-12 h-12 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                      type="number"
+                      name="inputField6"
+                      id="inputField6"
+                      max="1"
+                      onChange={handleChange}
+                      value={formData.inputField6}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+          </form>
         </div>
         <div className='w-full flex justify-center item-center'>
-          <button className='w-[90%] h-[30px] text-white bg-[#3E3B6A] border rounded-lg'>SAVE</button>
+          <button onClick={handleSaveClick} className='w-[90%] h-[30px] text-white bg-[#3E3B6A] border rounded-lg'>SAVE</button>
         </div>
       </div>
       <Friendsbar />
