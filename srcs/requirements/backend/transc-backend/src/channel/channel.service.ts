d4a330ecdd76dd1de62317f249a333e13ad4f526
Boumlik Brahim <92986664+Boumlik-Brahim@users.noreply.github.com>
2023-08-04 17:26:00 +0100

    EDIT: channel service

--- srcs/requirements/backend/transc-backend/src/channel/channel.service.ts
+++ srcs/requirements/backend/transc-backend/src/channel/channel.service.ts
@@ -24,14 +24,6 @@ export class ChannelService {
         cause: error
       });
     });
-    // await this.createChannelMember({
-    //   "userId": channel.channelOwnerId,
-    //   "channelId": channel.id,
-    //   "role": "OWNER",
-    //   "bannedTime": new Date(null),
-    //   "mutedTime": new Date(null)
-    // });
-    // return channel;
   }
   
   async findAllChannels(userId: string): Promise<Channel[]> {
@@ -118,12 +110,14 @@ export class ChannelService {
     });
   }
   
-  async updateChannel(id: string, updateChannelDto: UpdateChannelDto): Promise<Channel> {
+  async updateChannel(id: string, channelName: string): Promise<Channel> {
     return this.prisma.channel.update({
       where: {
         id
       },
-      data: updateChannelDto
+      data: {
+        channelName: channelName,
+      }
     })
     .catch (error => {
       throw new HttpException({
@@ -282,13 +276,11 @@ export class ChannelService {
 
   async updateChannelMemberMutedTime(channelId: string, userId: string, mutedTime: string): Promise<ChannelMember> {
     const unmuteTime = new Date();
-    console.log("current time", unmuteTime);
     const muteTime = new Date(mutedTime);
-    console.log("mute time", muteTime);
     if(muteTime.getMinutes() > unmuteTime.getMinutes())
     {
+      console.log('dkhl lhna');
       unmuteTime.setMinutes(unmuteTime.getMinutes() + (muteTime.getMinutes() - unmuteTime.getMinutes()));
-      console.log("unMute time", unmuteTime);
     }
     
     return this.prisma.channelMember.update({
@@ -317,8 +309,7 @@ export class ChannelService {
   @Cron(CronExpression.EVERY_30_SECONDS)
   async handleUnmuteMember(): Promise<void> {
     const currentDate = new Date();
-    // console.log(currentDate);
-    this.prisma.channelMember.updateMany({
+    await this.prisma.channelMember.updateMany({
       where: {
         role: 'MUTED_MEMBER',
         unmuteTime: {
@@ -488,44 +479,6 @@ export class ChannelService {
     });
   }
   
-  async findOneChannelMessage(channelId: string, memberId: string): Promise<ChannelMessage> {
-    return this.prisma.channelMessage.findUniqueOrThrow({
-      where: {
-        senderAndChannel: {
-          memberId,
-          channelId
-        },
-      }
-    })
-    .catch (error => {
-      throw new HttpException({
-        status: HttpStatus.NOT_FOUND,
-        error: 'NotFoundException',
-      }, HttpStatus.NOT_FOUND, {
-        cause: error
-      });
-    });
-  }
-
-  async removeChannelMessage(channelId: string, memberId: string): Promise<void> {
-    await this.prisma.channelMessage.delete({
-      where: {
-        senderAndChannel: {
-          memberId,
-          channelId
-        },
-      }
-    })
-    .catch (error => {
-      throw new HttpException({
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        error: 'InternalServerErrorException',
-      }, HttpStatus.INTERNAL_SERVER_ERROR, {
-        cause: error
-      });
-    });
-  }
-
   async removeAllChannelMessages(channelId: string): Promise<void> {
     await this.prisma.channelMessage.deleteMany({
       where: {
