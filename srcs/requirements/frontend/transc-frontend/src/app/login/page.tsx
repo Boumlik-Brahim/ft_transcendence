bf10ec77e7b7cb5fbacbb34279caa4688539445e
B-omayr <iomayr@student.1337.ma>
2023-08-07 18:50:09 +0100

    new: modify the logic of Login page(2Fa)

--- srcs/requirements/frontend/transc-frontend/src/app/login/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/login/page.tsx
@@ -22,8 +22,15 @@ export default function page()  {
       try{
         if (userId !== undefined) {
           const res = await axios.get(`http://localhost:3000/users/${userId}`);
-          if (res.data.isTwoFactorEnabled)
+          if (res.data.isTwoFactorEnabled){
             setTwoFa(true);
+            console.log("setted res.data.isTwoFactorEnabled -----+------");
+          }
+          if (!res.data.isTwoFactorEnabled && accessTokenCookie) {
+            return router.push(`/profile/${userId}`);
+          }
+          // if (res.data.isTwoFactorEnabled && !accessTokenCookie)
+          // setTwoFa(true);
         }
       }
       catch(e){
@@ -34,12 +41,57 @@ export default function page()  {
   },[])
   let showLogin = "";
   twoFa ? showLogin = "hidden" : showLogin = "";
-  useEffect(()=>{
-    if (accessTokenCookie) {
-      return router.push(`/profile/${userId}`);
-    }
+  
+  //------------------------------
+  const maxInputFields = 6;
+  const inputFieldIds = Array.from({ length: maxInputFields }, (_, index) => `inputField${index + 1}`);
+    
+  const [formData, setFormData] = useState({
+    inputField1: "",
+    inputField2: "",
+    inputField3: "",
+    inputField4: "",
+    inputField5: "",
+    inputField6: "",
+  });
+    
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
 
-  },[])
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const formDataValues = Object.values(formData);
+    const concatenatedString = formDataValues.join("");
+    try {
+      const response = await fetch(`http://localhost:3000/auth/2fa/authenticate?userId=${userId}&authCode=${concatenatedString}`, {
+        method: 'POST',
+      })
+      if (!response.ok) {
+        if (response.status === 401) {
+          const errorData = await response.json();
+          alert("Authentication failed: " + errorData.message);
+        } else {
+          console.log("Unexpected error occurred.");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setFormData({inputField1: "",
+    inputField2: "",
+    inputField3: "",
+    inputField4: "",
+    inputField5: "",
+    inputField6: ""})
+  };
+  //-----------------------------------------
   return (
     <div className={` w-full h-screen flex flex-col justify-around items-center bg-gray-200 py-[10%]`}>
       <div className="w-full flex justify-evenly items-center pr-[15%]">
@@ -71,7 +123,32 @@ export default function page()  {
           className=""
         />
       </div>
-      {twoFa && <PopUp />}
+      <div >
+        {twoFa && 
+         <div>
+         <form action="" method="post" onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+             <div className="w-[80%] flex flex-row items-center justify-evenly mx-auto
+               md:w-[100%]">
+               {inputFieldIds.map((fieldId, index) => (
+                 <div className="w-16 h-16" key={fieldId}>
+                   <input
+                     className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-sm bg-white focus:bg-gray-200 focus:ring-1 ring-[#3E3B6A]"
+                     type="text"
+                     name={fieldId}
+                     id={fieldId}
+                     pattern="^[0-9]$"
+                     value={formData[fieldId as keyof typeof formData]}
+                     onChange={handleChange}
+                   />
+                 </div>
+               ))}
+             </div>
+             <button type="submit" className="mt-4 bg-[#3E3B6A] text-white px-4 py-2 rounded-md">
+               Submit
+           </button>
+         </form>
+       </div>}
+      </div>
     </div>
   );
 }
