3601bd88bee3306fe307aab4a6c6674c8f229f80
B-omayr <iomayr@student.1337.ma>
2023-08-07 18:49:35 +0100

    new: complet twoFa page

--- srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/TwoFa/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/(dashboard)/TwoFa/page.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import QRCodePage from '../../../../components/QrCode';
 import Cookies from 'universal-cookie'
 import { useState } from "react";
+import Sidebar from "../../../../components/Sidebar";
+import Image from "next/image"
+import Link from "next/link"
 
 
 export default function TwoFa () {
@@ -23,7 +26,6 @@ export default function TwoFa () {
   const handleChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prevFormData) => {
-      console.log(e.target.name);
       return {
         ...prevFormData,
         [name]: value,
@@ -48,36 +50,58 @@ export default function TwoFa () {
         }
       }
     } catch (error) {
-      console.log("-------------Catch Error---------------", error);
+      console.log(error);
     }
+    setFormData({inputField1: "",
+    inputField2: "",
+    inputField3: "",
+    inputField4: "",
+    inputField5: "",
+    inputField6: ""})
   };
 
   return (
-    <div className="h-[80vh] flex flex-col items-center justify-start">
-      <div className="w-[50%]  flex justify-center item-center border-2 rounded-lg my-4">
-        <QRCodePage userId={userId} />
+    <>
+      <Sidebar/>
+      <div className="h-[80vh] w-[100%] flex flex-col items-center justify-start mt-[10%]
+        md:gap-8">
+        <div className="w-[250px] flex justify-center item-center border-2 rounded-lg my-4
+          md:w-[300px]">
+          <QRCodePage userId={userId} />
+        </div>
+        <div>
+          <form action="" method="post" onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+              <div className="w-[80%] flex flex-row items-center justify-evenly mx-auto
+                md:w-[100%]">
+                {inputFieldIds.map((fieldId, index) => (
+                  <div className="w-16 h-16" key={fieldId}>
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-sm bg-white focus:bg-gray-200 focus:ring-1 ring-[#3E3B6A]"
+                      type="text"
+                      name={fieldId}
+                      id={fieldId}
+                      pattern="^[0-9]$"
+                      value={formData[fieldId as keyof typeof formData]}
+                      onChange={handleChange}
+                    />
+                  </div>
+                ))}
+              </div>
+              <button type="submit" className="mt-4 bg-[#3E3B6A] text-white px-4 py-2 rounded-md">
+                Submit
+            </button>
+          </form>
+        </div>
+        <div className="mt-4">
+          <Link href="/setting">
+              <Image
+              src={'/HOME.svg'}
+              width={32}
+              height={32}
+              alt="Go Back"/>
+          </Link>
+        </div>
       </div>
-      <div>
-        <form action="" method="post" onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
-            <div className="w-[80%] flex flex-row items-center justify-evenly mx-auto no-scrollbar">
-              {inputFieldIds.map((fieldId, index) => (
-                <div className="w-16 h-16" key={fieldId}>
-                  <input
-                    className=" no-scrollbar w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-sm bg-white focus:bg-gray-200 focus:ring-1 ring-[#3E3B6A]"
-                    type="number"
-                    name={fieldId}
-                    id={fieldId}
-                    value={formData[fieldId as keyof typeof formData]}
-                    onChange={handleChange}
-                  />
-                </div>
-              ))}
-            </div>
-            <button type="submit" className="mt-4 bg-[#3E3B6A] text-white px-4 py-2 rounded-md">
-              Submit
-          </button>
-        </form>
-      </div>
-    </div>
+    </>
   );
 }
