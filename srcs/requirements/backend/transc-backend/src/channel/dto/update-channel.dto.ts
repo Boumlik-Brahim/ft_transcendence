d74b17fe592d1966b444d6f06d37983b29e6282e
Boumlik Brahim <92986664+Boumlik-Brahim@users.noreply.github.com>
2023-08-07 18:24:21 +0100

    EDIT: update channel dto

--- srcs/requirements/backend/transc-backend/src/channel/dto/update-channel.dto.ts
+++ srcs/requirements/backend/transc-backend/src/channel/dto/update-channel.dto.ts
@@ -3,7 +3,7 @@ import { CreateChannelDto } from './create-channel.dto';
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class UpdateChannelDto extends PartialType(CreateChannelDto) {
+export class UpdateChannelDto{
 
     @ApiProperty()
     @IsString()
