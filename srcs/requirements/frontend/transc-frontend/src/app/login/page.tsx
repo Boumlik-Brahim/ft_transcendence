5439e81c00d807088c01e9a13cfaa6affa4d9494
B-omayr <iomayr@student.1337.ma>
2023-08-14 18:33:50 +0100

    done: finished the login page logic with Reponsive

--- srcs/requirements/frontend/transc-frontend/src/app/login/page.tsx
+++ srcs/requirements/frontend/transc-frontend/src/app/login/page.tsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 import Image from 'next/image'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import PopUp from '../../../components/Popup'
 import  Cookies  from 'universal-cookie';
+import { useMediaQuery } from 'react-responsive';
+import TwoFa from '../(dashboard)/twofa/page';
+import TwoFaForm from '../../../components/TwoFaForm';
 
 
 export default function page()  {
@@ -19,9 +21,12 @@ export default function page()  {
   useEffect(() => {
     async function checkTwoFa() {
       try{
+        console.log("userId : ", userId);
         if (userId !== undefined) {
+          console.log("we will fetch now ********", userId);
           const res = await axios.get(`http://localhost:3000/users/${userId}`);
           if (res.data.isTwoFactorEnabled){
+            console.log("from set fetch -------------------: ", res.data.isTwoFactorEnabled);
             setTwoFa(true);
           }
           if (!res.data.isTwoFactorEnabled && accessTokenCookie) 
@@ -36,119 +41,98 @@ export default function page()  {
   },[])
   let showLogin = "";
   twoFa ? showLogin = "hidden" : showLogin = "";
-  //------------------------------
-  const maxInputFields = 6;
-  const inputFieldIds = Array.from({ length: maxInputFields }, (_, index) => `inputField${index + 1}`);
-    
-  const [formData, setFormData] = useState({
-    inputField1: "",
-    inputField2: "",
-    inputField3: "",
-    inputField4: "",
-    inputField5: "",
-    inputField6: "",
-  });
-    
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [name]: value,
-      };
-    });
-  };
+  //^ ---------------------------  screen sizes states ----------------------------
+  const isLgScreen = useMediaQuery({ minWidth: 1200 });
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const formDataValues = Object.values(formData);
-    const concatenatedString = formDataValues.join("");
-    try {
-      const response = await fetch(`http://localhost:3000/auth/2fa/authenticate?userId=${userId}&authCode=${concatenatedString}`, {
-        method: 'POST',
-      })
-      console.log(response);
-      if (!response.ok) {
-        if (response.status === 401) {
-          const errorData = await response.json();
-          alert("Authentication failed: " + errorData.message);
-        } else {
-          console.log("Unexpected error occurred.");
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    setFormData({inputField1: "",
-    inputField2: "",
-    inputField3: "",
-    inputField4: "",
-    inputField5: "",
-    inputField6: ""})
-  };
-  //-----------------------------------------
+  const [isLgScreenState, setIsLgScreen] = useState(false);
+  //* useEffect for screen sizes state
+  useEffect(() => {
+    setIsLgScreen(isLgScreen);
+  }, [isLgScreen]);
+  //^ -------------------------------------------------------------------------------
   return (
-    <div className={` w-full h-screen flex flex-col justify-around items-center bg-gray-200 py-[10%]`}>
-      <div className="w-full flex justify-evenly items-center pr-[15%]">
-        <Link href="/" className="">
-          <Image src={"/HOME.svg"} width={25} height={25} alt="" className='md:w-[40px] md:h-[40px]' />
-        </Link>
-        <h2 className="font-press text-[#3E3B6A] text-[28px] md:text-[50px]">Login</h2>
+    <>
+      {(isLgScreenState) && (<>
+        <div className='h-[20vh] flex justify-center items-center bg-gray-200 font-press text-[40px] text-primary '>
+          <p className='w-[520px] h-[44px]'>Welcome Back!</p>
+        </div></>)
+      }  
+      <div className={` w-full h-screen flex flex-col justify-around items-center bg-gray-200 py-[10%]
+        lg:h-[80vh] lg:flex-row-reverse`}>
+        <div className=' w-full h-[10vh] flex flex-col justify-around items-center 
+          lg:w-2/5 lg:h-full'>
+          <div className="w-full flex justify-evenly items-center pr-[15%]
+            lg:pr-[0%]">
+            <Link href="/" className="">
+              <Image src={"/HOME.svg"} width={25} height={25} alt="" className='md:w-[40px] md:h-[40px]' />
+            </Link>
+            <h2 className="font-press text-[#3E3B6A] text-[28px] md:text-[50px]">Login</h2>
+          </div>
+          <div>
+            {
+              isLgScreenState && (showLogin == "" ? (
+                <div
+                    className={`
+                        w-[200px] h-[50px] font-press text-[12px] rounded-full text-[#3E3B6A] bg-white flex items-center justify-between p-4
+                        md:w-[300px] md:h-[70px]`}
+                    >
+                    <Link 
+                      href="http://localhost:3000/auth"
+                      >
+                        <p className='md:text-[20px]'>Login With</p>
+                    </Link>
+                    <Image
+                      src={"../42Logo.svg"}
+                      width={30}
+                      height={30}
+                      alt=""
+                      className="md:w-[40px] md:h-[40px]"
+                    />
+                  </div>
+              ) : (
+                  <div >
+                    {twoFa && <TwoFaForm userId={userId}/>}
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className=''>
+          <Image
+            src={"../landing/CONTROLER.svg"}
+            width={400}
+            height={400}
+            alt=""
+            className="md:w-[700px] md:h-[700px] lg:w-[900px] lg:h-[900px]"
+          />
+        </div>
+        <div
+          className={`
+              ${showLogin} w-[200px] h-[50px] font-press text-[12px] rounded-full text-[#3E3B6A] bg-white flex items-center justify-between p-4
+                md:w-[300px] md:h-[70px]
+                lg:hidden`}
+        >
+          <Link 
+            href="http://localhost:3000/auth"
+            >
+              <p className='md:text-[20px]'>Login With</p>
+          </Link>
+          <Image
+            src={"../42Logo.svg"}
+            width={30}
+            height={30}
+            alt=""
+            className="md:w-[40px] md:h-[40px]"
+          />
+        </div>
+        <div >
+          {twoFa && 
+            <div className='lg:hidden'>
+              <TwoFaForm userId={userId}/>
+            </div>}
+        </div>
       </div>
-      <div className=''>
-        <Image
-          src={"../landing/CONTROLER.svg"}
-          width={400}
-          height={400}
-          alt=""
-          className="md:w-[700px] md:h-[700px]"
-        />
-      </div>
-      <div
-        className={`
-            ${showLogin} w-[200px] h-[50px] font-press text-[12px] rounded-full text-[#3E3B6A] bg-white flex items-center justify-between p-4
-              md:w-[300px] md:h-[70px]`}
-      >
-        <Link 
-          href="http://localhost:3000/auth"
-          >
-            <p className='md:text-[20px]'>Login With</p>
-        </Link>
-        <Image
-          src={"../42Logo.svg"}
-          width={30}
-          height={30}
-          alt=""
-          className="md:w-[40px] md:h-[40px]"
-        />
-      </div>
-      <div >
-        {twoFa && 
-         <div>
-         <form action="" method="post" onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
-             <div className="w-[80%] flex flex-row items-center justify-evenly mx-auto
-               md:w-[100%]">
-               {inputFieldIds.map((fieldId, index) => (
-                 <div className="w-16 h-16" key={fieldId}>
-                   <input
-                     className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-sm bg-white focus:bg-gray-200 focus:ring-1 ring-[#3E3B6A]"
-                     type="text"
-                     name={fieldId}
-                     id={fieldId}
-                     pattern="^[0-9]$"
-                     value={formData[fieldId as keyof typeof formData]}
-                     onChange={handleChange}
-                   />
-                 </div>
-               ))}
-             </div>
-             <button type="submit" className="mt-4 bg-[#3E3B6A] text-white px-4 py-2 rounded-md">
-               Submit
-           </button>
-         </form>
-       </div>}
-      </div>
-    </div>
+    </>
   );
 }
 
