5d4a14869269d95b4f216cb84c611181e4312dcf
B-omayr <iomayr@student.1337.ma>
2023-08-16 18:38:44 +0100

    new: set an middleware in next to protect route

---
+++ srcs/requirements/frontend/transc-frontend/src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export default function middleware(req: NextRequest, res: NextResponse) {
+    const path = req.nextUrl.pathname;
+    const isPublicPath = path === '/login';
+    const hasToken = req.cookies.has("accessToken");
+    const userId = req.cookies.get("id")?.value || "";
+
+    if (isPublicPath && hasToken){
+        return NextResponse.redirect(new URL(`/profile/${userId}`, req.nextUrl));
+    }
+    if (!hasToken && !isPublicPath){
+        return NextResponse.redirect(new URL('/login', req.nextUrl));
+    }
+}
+ 
+export const config = {
+  matcher: [
+    '/leaderboard',
+    '/setting',
+    '/twofa',
+    '/login',
+    '/channels',
+    '/achievements',
+    '/chat',
+    '/game',
+    '/play',
+    '/profile',
+  ],
+}
