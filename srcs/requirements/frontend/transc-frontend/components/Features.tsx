e7cd6227b0beb55c17407691891b4edc8cf2cf60
B-omayr <iomayr@student.1337.ma>
2023-08-21 15:10:07 +0100

    update: change the Feat comp to make a Transition

--- srcs/requirements/frontend/transc-frontend/components/Features.tsx
+++ srcs/requirements/frontend/transc-frontend/components/Features.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface Props{
     title : string ,
@@ -8,13 +10,32 @@ interface Props{
 }
 
 function InfoBox({title, paragraph,image, isChat} : Props) {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY >= 400) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   let changes  = "";
   {isChat ? changes = "lg:flex-row-reverse" : changes="lg:flex-row";}
+  
   return(
     <div className={`
       w-[full] h-[250px] flex flex-col items-center pt-[50px]
       md:h-[600px]
-      ${changes} lg:justify-evenly lg:h-[400px]`}>
+      ${isChat ? "lg:flex-row-reverse" : "lg:flex-row"} lg:justify-evenly lg:h-[400px]`}>
       <div className="text-center flex flex-col items-center">
         <h1 className="
           font-press text-[20px]
@@ -35,7 +56,8 @@ function InfoBox({title, paragraph,image, isChat} : Props) {
         width={332}
         height={332}
         alt=""
-        className={`hidden md:flex float-left`}
+        // className={`hidden md:flex float-left fade-section ${isVisible ? 'animate-fadeInFromLeft' : ''}`}
+        className={`hidden md:flex float-left fade-section ${isVisible ? (isChat ? 'animate-fadeInFromLeft' : 'animate-fadeInFromRight') : ''}`}
       />
     </div>
   )
